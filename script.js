b1cefda3c9351317e9242151e1f8b04149717c35
Artur Mauricio <arturmauricioss@outlook.com>
2026-02-28 21:20:12 -0300

    arrumando o contraste de cores

--- script.js
+++ script.js
@@ -6,36 +6,49 @@ function carregar(){
 
     var data = new Date()
     var hora = data.getHours()
-    hora = 20
-    msg.innerHTML=`Agora são ${hora} horas.`
+    var minutos = data.getMinutes()
+    msg.innerHTML=`${hora}:${minutos}`
+
+
+function texto_claro(){
+    titulo.style.color = 'white'
+    rodape.style.color = 'white'
+}
+function texto_escuro(){
+    titulo.style.color = 'black'
+    rodape.style.color = 'black'
+}
 
     if (hora<5){
         img.src = './assets/madrugada.png'
         document.body.style.background = '#1A1A1A' 
+        texto_claro()
     }
     else if(hora<7){
         img.src = './assets/amanhecer.png'
-        document.body.style.background = '#1A1A1A' 
+        document.body.style.background = '#f9b394'
+        texto_escuro()
+        
     }
     else if(hora<12){
         img.src = './assets/dia.png'
-        document.body.style.background = '#1A1A1A' 
+        document.body.style.background = '#4EA2EA'
+        texto_claro()
     }
     else if(hora<17){
         img.src = './assets/tarde.png'
-        document.body.style.background = '#1A1A1A' 
+        document.body.style.background = '#961b40'
+        texto_claro()
     }
     else if(hora<19){
         img.src = './assets/entardecer.png'
         document.body.style.background = '#FCE494' 
-        titulo.style.color = 'black'
-        rodape.style.color = 'black'
+        texto_escuro()
         
     }
     else{
         img.src = './assets/noite.png'
         document.body.style.background = '#12121e'
-        titulo.style.color = 'white'
-        rodape.style.color = 'white'
+        texto_claro()
     }
 }
